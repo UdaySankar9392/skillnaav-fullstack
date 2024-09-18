0a7a9a017110b123ea374106ff92f3534eae076a
saipraneethEdutechex <saipraneeth.t@edutechex.com>
2024-09-18 17:24:17 +0530

    Push code

--- frontend/src/WebApp/Flows/PartnerFlow/SignUpLogin/PartnerCreateAccount.js
+++ frontend/src/WebApp/Flows/PartnerFlow/SignUpLogin/PartnerCreateAccount.js
@@ -50,13 +50,12 @@ const PartnerCreateAccount = () => {
 
   return (
     <div className="flex flex-col lg:flex-row min-h-screen font-poppins">
-      <div className="hidden lg:flex lg:w-1/2 items-center justify-center">
-        <img
-          src={createAccountImage}
-          alt="Create Account"
-          className="w-full h-full object-cover rounded-lg"
-        />
+      <div className="hidden lg:flex lg:w-1/2 items-center justify-center bg-gradient-to-br from-blue-500 to-teal-400">
+        <div className="w-3/4 h-3/4 flex items-center justify-center">
+          <h1 className="text-white text-5xl font-extrabold">Partner Portal</h1>
+        </div>
       </div>
+
       <div className="flex flex-col items-center justify-center p-8 w-full lg:w-1/2 bg-white">
         <div className="w-full max-w-md flex flex-col justify-center min-h-screen lg:min-h-full">
           <h1 className="text-2xl font-semibold mb-6 text-center">
@@ -188,7 +187,7 @@ const PartnerCreateAccount = () => {
                 <button
                   type="submit"
                   disabled={isSubmitting}
-                  className="w-full bg-purple-500 text-white p-3 rounded-lg hover:bg-purple-600 mb-4"
+                  className="w-full bg-teal-500 text-white p-4 rounded-lg hover:bg-teal-600 transition-colors duration-300 shadow-md"
                 >
                   <a href="/partner-profile-form">Register</a>
                 </button>
@@ -229,7 +228,10 @@ const PartnerCreateAccount = () => {
 
           <p className="text-center text-gray-500 font-poppins font-medium text-base leading-6">
             Already have an account?{" "}
-            <Link to="/partner/login" className="text-blue-500 hover:underline">
+            <Link
+              to="/partner/login"
+              className="text-teal-500 hover:underline font-semibold"
+            >
               Login
             </Link>
           </p>
