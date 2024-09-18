3326e3af6a3f4d11fa594b0cd27c83564e2b5709
saipraneethEdutechex <saipraneeth.t@edutechex.com>
2024-09-18 11:14:56 +0530

    Push

--- frontend/src/WebApp/Flows/UserFlow/SignUpLogin/UserProfileBuilding/UserProfileForm.js
+++ frontend/src/WebApp/Flows/UserFlow/SignUpLogin/UserProfileBuilding/UserProfileForm.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+
 const UserProfileForm = () => {
   const [formData, setFormData] = useState({
     name: "",
@@ -11,6 +11,7 @@ const UserProfileForm = () => {
   const [isFormValid, setIsFormValid] = useState(false);
   const navigate = useNavigate();
 
+  // Validate form on every change
   useEffect(() => {
     const { name, dob, educationLevel, fieldOfStudy } = formData;
     if (name && dob && educationLevel && fieldOfStudy) {
@@ -34,20 +35,28 @@ const UserProfileForm = () => {
     }
   };
 
+  const handleSkip = () => {
+    navigate("/user-profile-picture");
+  };
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100 font-poppins">
       <div className="w-full max-w-xl p-8 space-y-6 bg-white shadow-md rounded-lg">
         <div className="space-y-4">
-          <div className="w-full w-450px h-12 p-3 bg-[#F9F0FF] border-b border-[#E6C4FB]">
-            <h2 className="textsize-16px font-bold text-gray-700">
+          <div className="w-full h-12 p-3 bg-[#F9F0FF] border-b border-[#E6C4FB]">
+            <h2 className="text-16px font-bold text-gray-700">
               BASIC INFORMATION
             </h2>
           </div>
           <div>
-            <label className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700"
+            >
               Name
             </label>
             <input
+              id="name"
               type="text"
               name="name"
               value={formData.name}
@@ -59,10 +68,14 @@ const UserProfileForm = () => {
             />
           </div>
           <div>
-            <label className="block text-sm font-medium text-gray-700">
+            <label
+              htmlFor="dob"
+              className="block text-sm font-medium text-gray-700"
+            >
               Date of Birth
             </label>
             <input
+              id="dob"
               type="date"
               name="dob"
               value={formData.dob}
@@ -70,14 +83,13 @@ const UserProfileForm = () => {
               className={`mt-1 block w-full px-3 py-2 border ${
                 formData.dob ? "border-gray-300" : "border-gray-200"
               } rounded-md shadow-sm focus:outline-none focus:ring-purple-500 focus:border-purple-500`}
-              placeholder="DD/MM/YYYY"
-              style={{ textTransform: "uppercase" }}
             />
           </div>
         </div>
+
         <div className="space-y-4">
-          <div className="w-full w-450px h-48px p-3 bg-[#F9F0FF] border-b border-[#E6C4FB]">
-            <h2 className="textsize-16px font-bold text-gray-700">
+          <div className="w-full h-12 p-3 bg-[#F9F0FF] border-b border-[#E6C4FB]">
+            <h2 className="text-16px font-bold text-gray-700">
               EDUCATIONAL INFORMATION
             </h2>
           </div>
@@ -98,7 +110,7 @@ const UserProfileForm = () => {
                 />
                 <label
                   htmlFor="highschool"
-                  className="ml-3 block text-sm text-gray-700"
+                  className="ml-3 mt-5 block text-sm text-gray-700"
                 >
                   Highschool
                 </label>
@@ -115,7 +127,7 @@ const UserProfileForm = () => {
                 />
                 <label
                   htmlFor="undergraduate"
-                  className="ml-3 block text-sm text-gray-700"
+                  className="ml-3 mt-5 block text-sm text-gray-700"
                 >
                   Undergraduate
                 </label>
@@ -132,7 +144,7 @@ const UserProfileForm = () => {
                 />
                 <label
                   htmlFor="graduate"
-                  className="ml-3 block text-sm text-gray-700"
+                  className="ml-3 mt-5  block text-sm text-gray-700"
                 >
                   Graduate
                 </label>
@@ -140,29 +152,32 @@ const UserProfileForm = () => {
             </div>
           </div>
           <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Field of study
+            <label
+              htmlFor="fieldOfStudy"
+              className="block text-sm mt-6 font-medium text-gray-700"
+            >
+              Field of Interest
             </label>
             <select
+              id="fieldOfStudy"
               name="fieldOfStudy"
               value={formData.fieldOfStudy}
               onChange={handleChange}
-              className={`mt-1 block w-full px-3 py-2 border ${
+              className={`mt-2 block w-full px-3 py-2 border ${
                 formData.fieldOfStudy ? "border-gray-300" : "border-gray-200"
               } bg-white rounded-md shadow-sm focus:outline-none focus:ring-purple-500 focus:border-purple-500`}
             >
               <option value="">Select Your Field</option>
-              <option value="CSE">CSE</option>
-              <option value="ECE">ECE</option>
-              <option value="MECH">MECH</option>
-              <option value="CIVIL">CIVIL</option>
-              <option value="IT">IT</option>
-              <option value="EEE">EEE</option>
-              {/* Add more options as needed */}
+              <option value="space">Space Internships</option>
+              <option value="aero">Aeronautical Internships</option>
+              <option value="tech">Tech Internships</option>
+              <option value="research">Research Internships</option>
+              <option value="education">Education Internships</option>
             </select>
           </div>
         </div>
-        <div>
+
+        <div className="flex space-x-4">
           <button
             type="button"
             onClick={handleSubmit}
@@ -173,7 +188,14 @@ const UserProfileForm = () => {
                 : "bg-purple-300 cursor-not-allowed"
             } focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500`}
           >
-            <a href="/user-profile-picture">Continue</a>
+            Continue
+          </button>
+          <button
+            type="button"
+            onClick={handleSkip}
+            className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-500 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400"
+          >
+            Skip
           </button>
         </div>
       </div>
