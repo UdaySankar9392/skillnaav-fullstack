a9b7f67558272aeda1ab0fb01838ea3572dc2068
saipraneethEdutechex <saipraneeth.t@edutechex.com>
2024-09-20 14:58:56 +0530

    Push changes

--- frontend/src/WebApp/Flows/AdminFlow/SignUpLogin/AdminLogin.js
+++ frontend/src/WebApp/Flows/AdminFlow/SignUpLogin/AdminLogin.js
@@ -6,6 +6,7 @@ import axios from "axios";
 import Loading from "../../../Warnings/Loading/Loading";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import adminImage from "../../../../assets-webapp/partner.jpg";
 
 // Validation schema for Formik
 const validationSchema = Yup.object({
@@ -41,134 +42,146 @@ const AdminLogin = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen font-poppins bg-gray-100">
-      <div className="w-full max-w-md bg-white p-8 shadow-lg rounded-lg">
-        <h1 className="text-3xl font-extrabold mb-4 text-center text-gray-800">
-          Welcome Back
-        </h1>
-        <h2 className="text-lg font-medium mb-6 text-center text-gray-600">
-          Please sign in to your account
-        </h2>
+    <div className="flex flex-col lg:flex-row min-h-screen font-poppins">
+      {/* Left Side Image */}
+      <div className="hidden lg:flex lg:w-1/2 items-center justify-center">
+        <img
+          src={adminImage}
+          alt="Admin Login"
+          className="w-full h-full object-cover"
+        />
+      </div>
 
-        {/* Error Message */}
-        {error && (
-          <div className="bg-red-200 text-red-600 p-3 mb-4 text-center rounded-lg">
-            {error}
-          </div>
-        )}
+      {/* Right Side Login Form */}
+      <div className="flex flex-col items-center justify-center p-8 w-full lg:w-1/2 bg-white">
+        <div className="w-full max-w-md">
+          <h1 className="text-3xl font-extrabold mb-4 text-center text-gray-800">
+            Welcome Back
+          </h1>
+          <h2 className="text-lg font-medium mb-6 text-center text-gray-600">
+            Please sign in to your account
+          </h2>
 
-        {/* Loading */}
-        {loading ? (
-          <Loading />
-        ) : (
-          <Formik
-            initialValues={{ email: "", password: "" }}
-            validationSchema={validationSchema}
-            onSubmit={handleSubmit}
-          >
-            {({ isSubmitting }) => (
-              <Form className="space-y-4">
-                {/* Email Field */}
-                <div className="relative">
-                  <Field
-                    type="email"
-                    name="email"
-                    placeholder="Enter your email"
-                    className="w-full p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-teal-400"
-                    aria-label="Email"
-                  />
-                  <ErrorMessage
-                    name="email"
-                    component="div"
-                    className="text-red-500 text-sm mt-1"
-                  />
-                </div>
+          {/* Error Message */}
+          {error && (
+            <div className="bg-red-200 text-red-600 p-3 mb-4 text-center rounded-lg">
+              {error}
+            </div>
+          )}
 
-                {/* Password Field */}
-                <div className="relative">
-                  <Field
-                    type={showPassword ? "text" : "password"}
-                    name="password"
-                    placeholder="Enter your password"
-                    className="w-full p-4 pr-16 border border-gray-300 rounded-lg focus:ring-2 focus:ring-teal-400"
-                    aria-label="Password"
-                  />
-                  <button
-                    type="button"
-                    className="absolute inset-y-0 right-4 flex items-center"
-                    onClick={() => setShowPassword(!showPassword)}
-                    aria-label={
-                      showPassword ? "Hide Password" : "Show Password"
-                    }
-                  >
-                    <FontAwesomeIcon
-                      icon={showPassword ? faEyeSlash : faEye}
-                      size="lg"
-                      className="text-gray-600 mt-5"
-                    />
-                  </button>
-                  <ErrorMessage
-                    name="password"
-                    component="div"
-                    className="text-red-500 text-sm mt-1"
-                  />
-                </div>
-
-                {/* Remember Me and Forgot Password */}
-                <div className="flex justify-between items-center mb-6">
-                  <div className="flex items-center space-x-2">
+          {/* Loading */}
+          {loading ? (
+            <Loading />
+          ) : (
+            <Formik
+              initialValues={{ email: "", password: "" }}
+              validationSchema={validationSchema}
+              onSubmit={handleSubmit}
+            >
+              {({ isSubmitting }) => (
+                <Form className="space-y-4">
+                  {/* Email Field */}
+                  <div className="relative">
                     <Field
-                      type="checkbox"
-                      name="rememberMe"
-                      id="rememberMe"
-                      className="form-checkbox h-4 w-4 text-teal-500 transition duration-150 ease-in-out"
+                      type="email"
+                      name="email"
+                      placeholder="Enter your email"
+                      className="w-full p-4 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400"
+                      aria-label="Email"
                     />
-                    <label
-                      htmlFor="rememberMe"
-                      className="block text-sm mt-5 text-gray-700"
-                    >
-                      Remember me
-                    </label>
+                    <ErrorMessage
+                      name="email"
+                      component="div"
+                      className="text-red-500 text-sm mt-1"
+                    />
                   </div>
 
-                  <Link
-                    to="/forgot-password"
-                    className="text-sm mt-5 font-medium text-teal-500 hover:text-teal-700 transition duration-150 ease-in-out"
+                  {/* Password Field */}
+                  <div className="relative">
+                    <Field
+                      type={showPassword ? "text" : "password"}
+                      name="password"
+                      placeholder="Enter your password"
+                      className="w-full p-4 pr-16 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400"
+                      aria-label="Password"
+                    />
+                    <button
+                      type="button"
+                      className="absolute inset-y-0 right-4 flex items-center"
+                      onClick={() => setShowPassword(!showPassword)}
+                      aria-label={
+                        showPassword ? "Hide Password" : "Show Password"
+                      }
+                    >
+                      <FontAwesomeIcon
+                        icon={showPassword ? faEyeSlash : faEye}
+                        size="lg"
+                        className="text-gray-600 mt-5"
+                      />
+                    </button>
+                    <ErrorMessage
+                      name="password"
+                      component="div"
+                      className="text-red-500 text-sm mt-1"
+                    />
+                  </div>
+
+                  {/* Remember Me and Forgot Password */}
+                  <div className="flex justify-between items-center mb-6">
+                    <div className="flex items-center space-x-2">
+                      <Field
+                        type="checkbox"
+                        name="rememberMe"
+                        id="rememberMe"
+                        className="form-checkbox h-4 w-4 text-blue-500 transition duration-150 ease-in-out"
+                      />
+                      <label
+                        htmlFor="rememberMe"
+                        className="block text-sm mt-5 text-gray-700"
+                      >
+                        Remember me
+                      </label>
+                    </div>
+
+                    <Link
+                      to="/admin/login"
+                      className="text-sm mt-5 font-medium text-blue-500 hover:text-blue-700 transition duration-150 ease-in-out"
+                    >
+                      Forgot password?
+                    </Link>
+                  </div>
+
+                  {/* Sign In Button */}
+                  <button
+                    type="submit"
+                    disabled={isSubmitting}
+                    className="w-full bg-blue-500 text-white p-4 rounded-lg hover:bg-blue-600 transition-colors duration-300 shadow-md"
                   >
-                    Forgot password?
-                  </Link>
-                </div>
+                    Sign In
+                  </button>
+                </Form>
+              )}
+            </Formik>
+          )}
 
-                {/* Sign In Button */}
-                <button
-                  type="submit"
-                  disabled={isSubmitting}
-                  className="w-full bg-teal-500 text-white p-4 rounded-lg hover:bg-teal-600 transition-colors duration-300 shadow-md"
-                >
-                  Sign In
-                </button>
-              </Form>
-            )}
-          </Formik>
-        )}
+          {/* Divider */}
+          <div className="flex items-center my-6">
+            <hr className="w-full border-gray-300" />
+            <span className="px-3 text-gray-500">OR</span>
+            <hr className="w-full border-gray-300" />
+          </div>
 
-        {/* Divider */}
-        <div className="flex items-center my-6">
-          <hr className="w-full border-gray-300" />
-          <span className="px-3 text-gray-500">OR</span>
-          <hr className="w-full border-gray-300" />
+          {/* Sign Up */}
+          <p className="text-center text-gray-500">
+            Don’t have an account?{" "}
+            <Link
+              to="/admin-create-account"
+              className="text-blue-500 hover:underline font-semibold"
+            >
+              Sign up
+            </Link>
+          </p>
         </div>
-
-        {/* Sign Up */}
-        <p className="text-center text-gray-500">
-          Don’t have an account?{" "}
-          <Link
-            to="/admin-create-account"
-            className="text-teal-500 hover:underline font-semibold"
-          >
-            Sign up
-          </Link>
-        </p>
       </div>
     </div>
   );
